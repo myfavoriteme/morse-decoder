d96a58293743242f0825eb4434d7b60df34e2fa2
Vladislav Kakorin <vlad.kakorin03@gmail.com>
2023-01-21 13:20:32 +0300

    the task was solved

--- src/index.js
+++ src/index.js
@@ -38,7 +38,22 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+
+    for(let i = 0; i < (expr.length); i = i+10) {
+        arr.push(expr.slice(i, i + 10));
+        
+    }
+
+    arr = arr.map(function(item){
+        return item.replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-').replace(/\*\*\*\*\*\*\*\*\*\*/g, ' ');
+    })
+    
+    arr = arr.map(item => item != ' ' ? MORSE_TABLE[item] : ' ');
+
+    let out = arr.join('');
+
+    return  out;
 }
 
 module.exports = {
